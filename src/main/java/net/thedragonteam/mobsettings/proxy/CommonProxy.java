5ab22877fe4f21833e844b8a9e3ad7b1ca1b139d
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2016-12-18 01:55:56 +0200

    Added the ability to add and remove spawns for mobs
    Including spawnrates, min amount and max amount
    Made a config to depend if the drops should only drop when killed by player
    Added a few missed Vanilla Mobs
    Updated forge, mappings

--- src/main/java/net/thedragonteam/mobsettings/proxy/CommonProxy.java
+++ src/main/java/net/thedragonteam/mobsettings/proxy/CommonProxy.java
@@ -9,9 +9,15 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.thedragonteam.mobsettings.registry.MobDrops;
 import net.thedragonteam.thedragonlib.util.LogHelper;
 
+import static net.thedragonteam.mobsettings.registry.MobSpawns.addMobSpawns;
+import static net.thedragonteam.mobsettings.registry.MobSpawns.removeMobSpawns;
+
 public class CommonProxy {
+
     public void preInit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new MobDrops());
+        addMobSpawns();
+        removeMobSpawns();
         LogHelper.info("Finished PreInitialization");
     }
 }
